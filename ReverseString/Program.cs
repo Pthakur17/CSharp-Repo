ae3fc58b46bcae38b79eba7609237d889acb80bc
pthakur17 <priyanka.thakur1711@gmail.com>
2020-06-27 10:00:07 +0530

    Commit all file

---
+++ ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString;
+            Console.Write("Hello, enter a string :- ");
+			testString = Console.ReadLine();
+            var result = ReverseString.Reverse(testString);
+            Console.WriteLine("Reverse of String :- " + result);
+        }
+    }
+
+     //Reverse a string
+    public static class ReverseString
+    {
+        public static string Reverse(string x)
+        {
+            string result = "";
+            for (int i = x.Length - 1; i >= 0; i--)
+                result += x[i];
+            return result;
+        }
+    }
+}
